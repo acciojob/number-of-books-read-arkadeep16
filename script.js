144be1215946eea852150e5b0141722be03580ea
arkadeep16 <75775078+arkadeep16@users.noreply.github.com>
2023-10-14 16:03:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
+// const human = {
+//   name: 'Arka',
+//   age: 24,
+//   height: 5.8,
+//   weight: 55,
+//   skills: {
+//     primary_skills: 'html' + ' css' + ' bootstrap' + ' js',
+//     secondary_skills: 'java' + ' wordpress',
+//   },
+//   des() {
+//     console.log("I'll succeed");
+//   },
+// };
+
+// ===========================================================================
+
 const library = [
   {
-    author: "Bill Gates",
-    title: "The Road Ahead",
+    author: 'Bill Gates',
+    title: 'The Road Ahead',
     readingStatus: true,
   },
   {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
+    author: 'Steve Jobs',
+    title: 'Walter Isaacson',
     readingStatus: true,
   },
   {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
+    author: 'Suzanne Collins',
+    title: 'Mockingjay: The Final Book of The Hunger Games',
     readingStatus: false,
   },
 ];
 
-const numberOfBooksRead = () => {
-  // write your code here
-};
-
+function numberOfBooksRead() {
+  let count = 0;
+  for (let i = 0; i < library.length; i++) {
+    if (library[i].readingStatus == true) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(numberOfBooksRead());
 // Do not change the code below
 
 alert(numberOfBooksRead());
